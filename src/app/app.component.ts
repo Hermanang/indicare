85974cb05bba8f504e0b56976569fa5d267c06e0
Hermanang <tobnang1234@gmail.com>
2018-02-11 02:54:31 +0100

    Installation reusie de Primeng (Procedure d'installation normal comme dans la docs)| Application fonction sans erreur

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import {Message} from 'primeng/components/common/api';
+import {MessageService} from 'primeng/components/common/messageservice'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [MessageService]
 })
 export class AppComponent {
-  title = 'app';
+  msgs: Message[] = [];
+  constructor(private messageService: MessageService) {}
+
+  showSuccess() {
+      this.msgs = [];
+      this.msgs.push({severity: 'success', summary: 'Success Message', detail: 'Order submitted'});
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import {GrowlModule} from 'primeng/growl';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    GrowlModule
   ],
   providers: [],
   bootstrap: [AppComponent]
